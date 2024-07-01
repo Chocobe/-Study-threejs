6e32e81a2c0682cbe76101f2c6ce963e3c5b0ef7
Miles <kyw05171@gmail.com>
2024-07-02 00:08:58 +0900

    10-texture - TextureManager 실습

--- 02-three-journey/10-textures/src/script.js
+++ 02-three-journey/10-textures/src/script.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+
 /**
  * Texture
+ * 방법 1) `class Texture` 를 사용하여 `Material`에 적용하기
  */
-// 방법 1) `class Texture` 를 사용하여 `Material`에 적용하기
 // const image = new Image();
 // const texture = new THREE.Texture(image);
 // // 현재 Three.js 버전 `0.164` 에서는 colorSpace 를 `SRGBColorSpace` 로 설정해야, 원본 이미지와 유사한 색상이 렌더링된다.
@@ -16,23 +18,72 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // };
 // image.src = '/textures/door/color.jpg';
 
-// 방법 2) `class TextureLoader` 를 사용하여 `Material`에 적용하기
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load(
-    '/textures/door/color.jpg',
-    // 이미지 로딩 완료 callback
-    texture => {
-        texture.colorSpace = THREE.SRGBColorSpace;
-    },
-    // 진행 과정 callback
-    () => {
-        // `0` 또는 `100%`만 동작해서 실제로는 **사용하지 않는다고 함**
-    },
-    // 에러 callback
-    error => {
-        console.log('이미지 로딩 실패 - error: ', error);
-    }
-);
+
+
+/**
+ * Texture
+ * 방법 2) `class TextureLoader` 를 사용하여 `Material`에 적용하기
+ */
+// const textureLoader = new THREE.TextureLoader();
+// const texture = textureLoader.load(
+//     '/textures/door/color.jpg',
+//     // 이미지 로딩 완료 callback
+//     texture => {
+//         texture.colorSpace = THREE.SRGBColorSpace;
+//     },
+//     // 진행 과정 callback
+//     () => {
+//         // `0` 또는 `100%`만 동작해서 실제로는 **사용하지 않는다고 함**
+//     },
+//     // 에러 callback
+//     error => {
+//         console.log('이미지 로딩 실패 - error: ', error);
+//     }
+// );
+
+
+
+/**
+ * Texture
+ * 방법 3) LoadingManager 를 사용하여 로딩 event를 함께 사용하기
+ * => 다양한 모델, 폰트, 텍스쳐를 로딩할 때 발생하는 많은 이벤트를 관리할 수 있다.
+ */
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+    console.log('onStart()');
+};
+loadingManager.onLoad = () => {
+    console.log('onLoad()');
+};
+loadingManager.onProgress = () => {
+    console.log('onProgress()');
+};
+loadingManager.onError = () => {
+    console.log('onError()');
+};
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load('/textures/door/color.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const heightTexture = textureLoader.load('/textures/door/height.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const normalTexture = textureLoader.load('/textures/door/normal.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg', texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+});
 
 /**
  * Base
@@ -48,7 +99,7 @@ const scene = new THREE.Scene()
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({
-    map: texture,
+    map: colorTexture,
 });
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
