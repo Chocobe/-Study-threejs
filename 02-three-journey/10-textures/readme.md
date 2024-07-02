5f0b807a62065ca8f0284299d9789125da98f9d5
Miles <kyw05171@gmail.com>
2024-07-03 00:33:17 +0900

    10-texture 실습

--- 02-three-journey/10-textures/readme.md
+++ 02-three-journey/10-textures/readme.md
@@ -14,3 +14,82 @@ npm run dev
 # Build for production in the dist/ directory
 npm run build
 ```
+
+
+
+<br /><hr /><br />
+
+
+
+# 1. Texture를 사용할 때, 최적화 하는 방법 3가지 포인트
+
+* File 용량이 작을수록 좋다.
+* 이미지 해상도
+* Texture data
+
+<br />
+
+## 1-1. File 용량이 작을수록 좋다.
+
+`.jpg` 는 **lossy compresion(손실 압축)` 을 하는 대신, 용량이 작다. 
+`.png` 는 **lossless compresion(무손실 압축)` 을 하는 대신, 용량이 크다.
+
+그러므로, `.png` 이미지를 사용한다면, `.jpg` 로 변환해서 사용하는 것이 좋다.
+
+* [`.jpg` => `.png` 변환 사이트](https://tinypng.com/)
+
+<br />
+
+## 1-2. 이미지 해상도 1
+
+`Texture` 는 `GPU` 로 전달하여 사용된다.
+
+해상도가 큰 이미지(`Texture`)는 `GPU` 에 부담이 된다. (`GPU` 에 전달할 수 없을수도 있다.)
+
+그래서 **가능한 작은 해상도 이미지** 를 `Texture` 로 사용하는 것이 좋다.
+
+<br />
+
+## 1-3. 이미지 해상도 2
+
+`Texture` 를 사용할 때, `Texture.generateMips = false` 로 직접 `MipMapping` 을 미사용 처리하지 안흔 한, `MipMapping` 동작인 **원본 이미지의 절반씩 줄여나가는 버전들** 을 생성하게 된다.
+
+만약 이미지 해상도가 2로 나누어 떨어지지 않는 값이라면, `GPU` 는 생성한 `MipMapping` 이미지를 늘려서 렌더링하게 되며, 이로 인해 이미지가 늘어난 것처럼 렌더링되는 현상이 발생한다.
+
+그러므로, 2의 배수의 값을 해상도로 가지는 이미지를 사용하는 것이 좋다.
+
+* 512 * 512
+* 1024 * 1024
+* 512 * 2048
+
+그리고 브라우저 콘솔에 에러가 출력될 것이다.
+
+<br />
+
+## 1-4 Texture data 1
+
+투명(`transparency`) 를 위해 `png` 를 사용한다면, 이는 `Alpha Texture` 와 함께 `png` 이미지 2개로 처리할 수 있다고 한다.
+
+`png` 파일을 2개 사용하게 되지만, `jpg` 보다 용량이 작아서 성능에서 이점이 생긴다고 한다.
+
+<br />
+
+## 1-5 Texture data 2
+
+`Normal Texture` 를 사용하게 될 경우, 이미지의 정확한 좌표값이 필요하다고 한다.
+
+때문에 `손실 이미지` 인 `png` 를 사용하면, 빛 반사나 그림자 등에서 원하는 결과가 나오지 않는다고 한다.
+
+때문에 `Normal Texture` 를 사용할 경우에는 `jpg` 이미지를 사용한다고 한다.
+
+(그렇다고 `png` 를 `jpg` 로 변환하게 되면, 이미 손실된 압축을 사용하게 되므로, `GPU` 에서 렌더링한 결과는 `png` 와 같은 원치않는 결과가 렌더링 된다.)
+
+
+
+<br /><hr /><br />
+
+
+
+## 2. Texture 이미지 제작
+
+Adobe 의 [Substance 3D Designer](https://www.adobe.com/kr/products/substance3d-designer.html?promoid=FHRLZ9BG&mv=other) 으로 Texture 용 이미지를 직접 만들 수 있다.
--- 02-three-journey/10-textures/src/script.js
+++ 02-three-journey/10-textures/src/script.js
@@ -63,7 +63,16 @@ const loadingManager = new THREE.LoadingManager();
 // };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load('/textures/door/color.jpg', texture => {
+// const colorTexture = textureLoader.load('/textures/door/color.jpg', texture => {
+//     texture.colorSpace = THREE.SRGBColorSpace;
+// });
+// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png', texture => {
+//     texture.colorSpace = THREE.SRGBColorSpace;
+// });
+// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png', texture => {
+//     texture.colorSpace = THREE.SRGBColorSpace;
+// });
+const colorTexture = textureLoader.load('/textures/minecraft.png', texture => {
     texture.colorSpace = THREE.SRGBColorSpace;
 });
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg', texture => {
@@ -123,7 +132,7 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg', text
  * => `UV coordinator`의 원점(0, 0)은 좌측 하단 점이다.
  * => 그러므로 좌측 하단의 원전(0, 0)에서 지정한 각도(radian) 만큼 회전한다. 
  */
-colorTexture.rotation = Math.PI / 4;
+// colorTexture.rotation = Math.PI / 4;
 
 /**
  * `Texture.center.축`
@@ -132,8 +141,44 @@ colorTexture.rotation = Math.PI / 4;
  * 
  * => 아래의 (0.5, 0.5) 설정은, `Geometry`의 크기가 `width: 1`, `height: 1` 이기 때문에, 중심점 좌표값을 지정한 것이다.
  */
-colorTexture.center.x = 0.5;
-colorTexture.center.y = 0.5;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+/**
+ * Filtering & MipMapping
+ * => Texture에 로드한 이미지의 원본 크기에서 절반씩 작은 버전의 이미지를 Three.js 내부적으로 만들어내고,
+ * => 실제 렌더링되는 화면을 축소하여 렌더링되는 Texture 크기가 작게 된다면, 해당 크기에 맞는 버전의 이미지가 사용된다.
+ * => 즉, 크기가 큰 원본 이미지를 그대로 사용하지 않고, 실제 렌더링되는 크기에 맞는 크기의 이미지를 사용하는 알고리즘이다.
+ * 
+ * => 프로젝트를 개발하면서 Texture.minMap 값을 바꿔보면서 원하는 렌더링 결과를 선택하는 정도로 사용하자.
+ */
+/**
+ * `Texture.minFilter`
+ * => 화면을 확대했을 때, Texture 를 어떻게 보여줄 것인가 설정
+ */
+/**
+ * `THREE.NearestFilter`
+ * => 화면을 확대했을 때(Texture를 사용하는 Mesh가 매우 크게 렌더링됨), blur 효과가 아니라 날카롭게 보이는듯?
+ * 
+ * => PC 성능을 덜 사용하므로, 성능 퍼포먼스나 Frame rate 에서 유리하다.
+ * 
+ * => 아래와 같이 `minFilter = THREE.NearestFilter` 를 사용하는 것은 `MipMapping` 을 사용하지 않는것과 유사하다.
+ * => 그러므로, `MipMapping` 자체를 **미사용** 하는 것이 성능에서 유리핟다.
+ * => `MipMapping` 미사용 처리 방법
+ * => => `Texture.generateMipMaps = false`
+ */
+// colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.generateMipMaps = false;
+
+/**
+ * `Texture.magFilter`
+ * => magFilter 의 FullName 은, `Magnification Filter`
+ * => 화면을 축소했을 때(Texture를 사용하는 Mesh가 작게 렌더링됨) 또는,
+ * => Texture를 사용한 이미지의 크기가 매우 작을 경우,
+ * => 각 픽셀의 색상 경계를 blur 처리할 것인지, 마인크래프트처럼 날카롭게 구분할 것인지 설정
+ */
+colorTexture.magFilter = THREE.NearestFilter;
+
 
 
 /**
