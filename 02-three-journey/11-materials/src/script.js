953c995eecf2929ca57aa83918225196a2dd3eb4
Miles <kyw05171@gmail.com>
2024-07-05 00:45:33 +0900

    11-Materials - MeshLambertMaterial 실습

--- 02-three-journey/11-materials/src/script.js
+++ 02-three-journey/11-materials/src/script.js
@@ -11,6 +11,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
  * Texture
  */
 const textureLoader = new THREE.TextureLoader();
@@ -22,19 +34,114 @@ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
-const mapcapTexture = textureLoader.load('/textures/mapcaps/1.png');
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
 const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
 
 doorColorTexture.colorSpace = THREE.SRGBColorSpace;
-mapcapTexture.colorSpace = THREE.SRGBColorSpace;
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Objects
  */
-// MeshBasicMaterial
-const material = new THREE.MeshBasicMaterial({
-    map: doorColorTexture,
-});
+/**
+ * MeshBasicMaterial
+ * 
+ * 기본 Material
+ */
+function initMeshBasicMaterial() {
+    // const material = new THREE.MeshBasicMaterial({
+    //     map: doorColorTexture,
+    // });
+    // const material = new THREE.MeshBasicMaterial();
+    // material.map = doorColorTexture;
+    
+    // const material = new THREE.MeshBasicMaterial({
+    //     color: 'green',
+    // });
+
+    const material = new THREE.MeshBasicMaterial();
+    // material.color = new THREE.Color('green');
+    material.color = new THREE.Color(0xff0000);
+    
+    // material.wireframe = true;
+    
+    // material.transparent = true;
+    // material.opacity = 0.2;
+    // material.alphaMap = doorAlphaTexture;
+    
+    // material.side = THREE.DoubleSide;
+    // material.side = THREE.FrontSide;
+    // material.side = THREE.BackSide;
+    return material;
+}
+// const material = initMeshBasicMaterial();
+
+/**
+ * MeshNormalMaterial
+ * 
+ * 각 `vertex(점)`은 카메라를 기준으로 색상이 렌더링된다.
+ * => 그래서 `Sphere` 를 대상으로 카메라를 회전시켜 보면, `Sphere` 를 보는 방향이 바뀌어도 렌더링되는 색상은 변하지 않는다.
+ * => `shadow`를 생성하는 `Light`가 있더라도, 오직 카메라를 기준으로만 렌더링된다.
+ */
+function initMeshNormalMaterial() {
+    const material = new THREE.MeshNormalMaterial();
+    material.wireframe = true;
+
+    return material;
+}
+// const material = initMeshNormalMaterial();
+
+/**
+ * MeshMapcapMaterial
+ * 
+ * 카메라를 기준으로 Mesh의 좌표를 `Texture`에서 동일한 좌표에 위치한 색상을 렌더링하는 방식이다.
+ * => 성능을 적게 쓰므로, 퍼포먼스가 좋다고 한다.
+ * 
+ * `Texture`에 사용한 이미지에 명암이 표현되어 있다면,
+ * => `Light` 객체를 사용하지 않아도 **마치 그림자가 생기는 것 처럼(카메라 기준 고정된 위치에 생김)**,
+ * => 낮은 성능을 사용하여 그림자를 연출할 수 있다.
+ */
+function initMeshMatcapMaterial() {
+    const material = new THREE.MeshMatcapMaterial();
+    material.matcap = matcapTexture;
+
+    return material;
+}
+// const material = initMeshMatcapMaterial();
+
+/**
+ * MeshDepthMaterial
+ * 
+ * 카메라와 거리가 가까우면 `흰색`, 멀면 `검은색`에 가까운 색상이 렌더링된다.
+ * 
+ * 일반적으로 `MeshDepthMaterial` 을 직접 사용하지는 않는다.
+ * => Three.js 내부에서 사용되고 있는 Material 이다.
+ * => `post-processing`
+ */
+function initMeshDepthMaterial() {
+    const material = new THREE.MeshDepthMaterial();
+
+    return material;
+}
+// const material = initMeshDepthMaterial();
+
+/**
+ * MeshLambertMaterial
+ * 
+ * `Light`가 꼭 필요하다.
+ * => `Light`가 없으면 렌더링되지 않는다.
+ * 
+ * `Light`를 사용한 명암 연출에 낮은 성능을 사용하므로, 퍼포먼스가 좋다.
+ * => 하지만, 현식적이지 못한 이상한 결과가 나와서, 이 현상을 해소하기는 어렵다고 한다.
+ */
+function initMeshLambertMaterial() {
+    const material = new THREE.MeshLambertMaterial();
+
+    return material;
+}
+const material = initMeshLambertMaterial();
+
+
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
