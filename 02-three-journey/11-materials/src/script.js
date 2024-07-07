5c698c66681e09280975c8422a051f6bcb48b774
Miles <kyw05171@gmail.com>
2024-07-07 09:14:52 +0900

    11-Materials - MeshPhysicalMaterial 실습

--- 02-three-journey/11-materials/src/script.js
+++ 02-three-journey/11-materials/src/script.js
@@ -223,59 +223,72 @@ function initMeshStandardMaterial() {
 
     material.map = doorColorTexture;
 
-    // `aoMap`: Ambient Occlusion Map
-    // => `aoMap` 에 설정한 `Texture` 는 `Ambient Light` 이외의 `Light` 에는 영향을 받지 않는다.
-    // => 때문에, `scene.environment` 에 설정한 `HRD` 가 `aoMap`에는 영향을 주지 않는다.
-    //
-    // => 결과적으로 `doorAmbientOcclusionTexture` 이미지에서 검은 색에 가까운 테두리 부분이 진하게 렌더링된다.
+    /**
+     * `aoMap`: Ambient Occlusion Map
+     * => `aoMap` 에 설정한 `Texture` 는 `Ambient Light` 이외의 `Light` 에는 영향을 받지 않는다.
+     * => 때문에, `scene.environment` 에 설정한 `HRD` 가 `aoMap`에는 영향을 주지 않는다.
+     * 
+     * => 결과적으로 `doorAmbientOcclusionTexture` 이미지에서 검은 색에 가까운 테두리 부분이 진하게 렌더링된다.
+     * 
+     */
     material.aoMap = doorAmbientOcclusionTexture;
     material.aoMapIntensity = 1;
 
-    // `displacementMap`
-    // => `Texture`의 밝은 부분은 좌표 고도가 높아지고
-    // => 어두운 부분은 좌표 고도가 낮아진다.
-    // => 결과적으로 `Texture`에 의해, `Mesh`가 변형된다.
-    // 
-    // `displacementMap` === `heightMap` 같은 용어다.
-    //
-    // => `Mesh`를 변형하기 때문에,
-    // => `Geometry`에 충분히 많은 `subdivision`이 있어야 한다.
-    // material.displacementMap = doorHeightTexture;
-    // material.displacementScale = 1;
+    /**
+     * `displacementMap`
+     * => `Texture`의 밝은 부분은 좌표 고도가 높아지고
+     * => 어두운 부분은 좌표 고도가 낮아진다.
+     * => 결과적으로 `Texture`에 의해, `Mesh`가 변형된다.
+     * 
+     * `displacementMap` === `heightMap` 같은 용어다.
+     * 
+     * => `Mesh`를 변형하기 때문에,
+     * => `Geometry`에 충분히 많은 `subdivision`이 있어야 한다.
+     */
+    material.displacementMap = doorHeightTexture;
+    material.displacementScale = 1;
 
-    // `metalnessMap`
-    // => `Texture`의 밝은 부분은 금속 느낌으로 렌더링하고,
-    // => 어두운 부분은 고무 느낌으로 렌더링 한다.
-    //
-    // 만약 `MeshStandardMaterial.metalness`에 `1`이 아닌 다른 값이 함께 사용되었다면,
-    // => `곱 연산`이 되면서 이상한 결과를 보게 된다.
+    /**
+     * `metalnessMap`
+     * => `Texture`의 밝은 부분은 금속 느낌으로 렌더링하고,
+     * => 어두운 부분은 고무 느낌으로 렌더링 한다.
+     * 
+     * 만약 `MeshStandardMaterial.metalness`에 `1`이 아닌 다른 값이 함께 사용되었다면,
+     * => `곱 연산`이 되면서 이상한 결과를 보게 된다.
+     */
     material.metalnessMap = doorMetalnessTexture;
 
-    // `roughnessMap`
-    // => `Texture`의 밝은 부분은 부드러운 표면으로 렌더링하고,
-    // => 어두운 부분은 거칠게 렌더링한다.
-    //
-    // 만약 `MeshStandardMaterial.roughness`에 `1`이 아닌 다른 값이 함께 사용되었다면,
-    // => `곱 연산`이 되면서 이상한 결과를 보게 된다.
+    /**
+     * `roughnessMap`
+     * => `Texture`의 밝은 부분은 부드러운 표면으로 렌더링하고,
+     * => 어두운 부분은 거칠게 렌더링한다.
+     * 
+     * 만약 `MeshStandardMaterial.roughness`에 `1`이 아닌 다른 값이 함께 사용되었다면,
+     * => `곱 연산`이 되면서 이상한 결과를 보게 된다.
+     */
     material.roughnessMap = doorRoughnessTexture;
 
-    // `normalMap`
-    // => `displacementMap(=== heightMap)`처럼,
-    // => `Texture`의 밝은 부분은 튀어나오고,
-    // => 어두운 부분은 움푹 파인 느낌을 렌더링한다.
-    //
-    // `displacementMap`과 차이점은
-    // => `Geometry`의 좌표를 변형하지 않고,
-    // => 입체감을 표현한다는 것이다.
+    /**
+     * `normalMap`
+     * => `displacementMap(=== heightMap)`처럼,
+     * => `Texture`의 밝은 부분은 튀어나오고,
+     * => 어두운 부분은 움푹 파인 느낌을 렌더링한다.
+     * 
+     * `displacementMap`과 차이점은
+     * => `Geometry`의 좌표를 변형하지 않고,
+     * => 입체감을 표현한다는 것이다.
+     */
     material.normalMap = doorNormalTexture;
     // material.normalScale.set(1, 1);
     material.normalScale.set(0.5, 0.5);
 
-    // `alphaMap`
-    // => `Texture`의 밝은 부분은 `map`에 설정한 원본 `Texture`를 렌더링하고,
-    // => 어두운 부분은 투명하게 렌더링한다.
-    //
-    // `material.transparent = true` 설정이 있어야 투명효과가 렌더링된다.
+    /**
+     * `alphaMap`
+     * => `Texture`의 밝은 부분은 `map`에 설정한 원본 `Texture`를 렌더링하고,
+     * => 어두운 부분은 투명하게 렌더링한다.
+     * 
+     * `material.transparent = true` 설정이 있어야 투명효과가 렌더링된다.
+     */
     material.alphaMap = doorAlphaTexture;
     material.transparent = true;
 
@@ -284,9 +297,115 @@ function initMeshStandardMaterial() {
 
     return material;
 }
-const material = initMeshStandardMaterial();
+// const material = initMeshStandardMaterial();
 
+/**
+ * MeshPhysicalMaterial
+ * => `MeshStandardMaterial`을 확장한 class다.
+ * => 그러므로, `MeshStandardMaterial`의 모든 속성을 동일하게 제공한다.
+ */
+function initMeshPhysicalMaterial() {
+    const material = new THREE.MeshPhysicalMaterial();
+    // material.metalness = 0.7;
+    // material.metalness = 1;
 
+    // material.roughness = 0.2;
+    // material.roughness = 1;
+
+    // material.map = doorColorTexture;
+
+    // // `aoMap`: Ambient Occlusion Map
+    // material.aoMap = doorAmbientOcclusionTexture;
+    // material.aoMapIntensity = 1;
+
+    // // `displacementMap`
+    // // material.displacementMap = doorHeightTexture;
+    // // material.displacementScale = 1;
+
+    // // `metalnessMap`
+    // material.metalnessMap = doorMetalnessTexture;
+
+    // // `roughnessMap`
+    // material.roughnessMap = doorRoughnessTexture;
+
+    // // `normalMap`
+    // material.normalMap = doorNormalTexture;
+    // material.normalScale.set(0.5, 0.5);
+
+    // // `alphaMap`
+    // material.alphaMap = doorAlphaTexture;
+    // material.transparent = true;
+
+    // gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+    // gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+
+    /**
+     * `clearcoat`
+     * => `Light`에 대해여, 표면에 광택 효과를 준다.
+     * => 마치 얇은 유리에 감싸여 있는 느낌이다.
+     */
+    // material.clearcoat = 1;
+    // material.clearcoatRoughness = 0;
+    // gui.add(material, 'clearcoat').min(0).max(1).step(0.0001);
+    // gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001);
+
+    /**
+     * `sheen`
+     * => 설정에 따라, `플라스틱` 또는 `옷감` 느낌으로 연출한다.
+     * 
+     * 이 효과를 `Fresnel effect(프레넬 효과)` 라고 한다.
+     * => 렌더링 결과를 묘사하면, **테두리가 강조된 느낌** 이다.
+     * => 마치 물체 뒤쪽에 밝은 빛이 있어서, 물체의 테두리가 밝게 반사광이 보이는 현상이다.
+     */
+    // material.sheen = 1;
+    // material.sheenRoughness = 0.25;
+    // material.sheenColor.set(1, 1, 1);
+    // gui.add(material, 'sheen').min(0).max(1).step(0.0001);
+    // gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001);
+    // gui.addColor(material, 'sheenColor');
+
+    /**
+     * `iridescence`
+     * => 표면에 무지개 효과를 만들어준다.
+     * => CD-Rom 이나, 비눗방울에 빛이 비춰지는 효과이다.
+     * 
+     * ⭐️ `iridescenceIOR`은 현실적인 연출을 하기위한 **특별한 값의 범위** 를 갖는다.
+     * => 최소값: `1`
+     * => 최대값: `2.333`
+     * => 이 범위를 벗어나면, 이상하다...
+     */
+    // material.iridescence = 1;
+    // material.iridescenceIOR = 1;
+    // material.iridescenceThicknessRange = [100, 800];
+    // gui.add(material, 'iridescence').min(0).max(1).step(0.0001);
+    // gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.0001);
+    // gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1);
+    // gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1);
+
+    /**
+     * `transmission`
+     * => 유리나 반투명 플라스틱 같은 `Mesh`에 가려진 뒷 부분이 투과되어 보이는 효과를 연출한다.
+     * 
+     * `material.ior`은 `transmission`에 대한 `IOR(index of refraction: 굴절률)`이다.
+     * => `material.iridescenceIOR`과는 다르게, 최대값을 10까지 사용하는 듯 하다.
+     */
+    material.transmission = 1;
+    material.ior = 1.5;
+    material.thickness = 0.5;
+
+    material.metalness = 0;
+    material.roughness = 0;
+
+    gui.add(material, 'transmission').min(0).max(1).step(0.0001);
+    gui.add(material, 'ior').min(1).max(10).step(0.0001);
+    gui.add(material, 'thickness').min(0).max(1).step(0.0001);
+
+    gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+    gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+
+    return material;
+}
+const material = initMeshPhysicalMaterial();
 
 
 
