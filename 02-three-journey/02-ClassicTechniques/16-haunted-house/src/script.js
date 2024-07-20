0b086dea0a2dadc1f305ea60e41cb31b4ee56830
Miles <kyw05171@gmail.com>
2024-07-20 18:53:57 +0900

    16-Haunted house - Sky 실습

--- 02-three-journey/02-ClassicTechniques/16-haunted-house/src/script.js
+++ 02-three-journey/02-ClassicTechniques/16-haunted-house/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Sky } from 'three/addons/objects/Sky.js';
 import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
 
@@ -382,11 +383,26 @@ ghost1.shadow.camera.far = 10;
 
 ghost2.shadow.mapSize.width = 256;
 ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.near = 1;
 ghost2.shadow.camera.far = 10;
 
 ghost3.shadow.mapSize.width = 256;
 ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.near = 1;
 ghost3.shadow.camera.far = 10;
+
+/**
+ * Sky
+ */
+const sky = new Sky();
+sky.scale.setScalar(100);
+scene.add(sky);
+
+sky.material.uniforms.turbidity.value = 10;
+sky.material.uniforms.rayleigh.value = 3;
+sky.material.uniforms.mieCoefficient.value = 0.1;
+sky.material.uniforms.mieDirectionalG.value = 0.95;
+sky.material.uniforms.sunPosition.value.set(0.3, -0.038, -0.95);
 
 /**
  * Animate
