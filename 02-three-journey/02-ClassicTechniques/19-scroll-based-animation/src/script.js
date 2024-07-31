e634fed55a09cbcae7f3a737876e75c7882f81f5
Miles <kyw05171@gmail.com>
2024-07-31 22:25:07 +0900

    19-Scroll based animation - gradientTexture 실습

--- 02-three-journey/02-ClassicTechniques/19-scroll-based-animation/src/script.js
+++ 02-three-journey/02-ClassicTechniques/19-scroll-based-animation/src/script.js
@@ -12,6 +12,9 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor);
+    });
 
 /**
  * Base
@@ -25,9 +28,16 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg');
+gradientTexture.minFilter = THREE.NearestFilter;
+gradientTexture.magFilter = THREE.NearestFilter;
+
 // Material
 const material = new THREE.MeshToonMaterial({
     color: parameters.materialColor,
+    gradientMap: gradientTexture,
 });
 
 // Meshes
