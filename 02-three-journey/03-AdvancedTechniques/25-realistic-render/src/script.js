921a6c46a8ea814a2f8ca546966f5ee775fa4c65
Miles <kyw05171@gmail.com>
2024-09-28 17:44:09 +0900

    25-Realistic render 실습

--- 02-three-journey/03-AdvancedTechniques/25-realistic-render/src/script.js
+++ 02-three-journey/03-AdvancedTechniques/25-realistic-render/src/script.js
@@ -9,6 +9,7 @@ import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
  */
 const gltfLoader = new GLTFLoader()
 const rgbeLoader = new RGBELoader()
+const textureLoader = new THREE.TextureLoader();
 
 /**
  * Base
@@ -32,6 +33,8 @@ const updateAllMaterials = () =>
         if(child.isMesh)
         {
             // Activate shadow here
+            child.castShadow = true;
+            child.receiveShadow = true;
         }
     })
 }
@@ -57,19 +60,179 @@ rgbeLoader.load('/environmentMaps/0/2k.hdr', (environmentMap) =>
 })
 
 /**
+ * Directional light
+ */
+const directionalLight = new THREE.DirectionalLight(
+    '#fff',
+    6
+);
+directionalLight.position.set(-4, 6.5, 2.5);
+scene.add(directionalLight);
+
+gui
+    .add(directionalLight, 'intensity')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('lightIntensity');
+
+gui
+    .add(directionalLight.position, 'x')
+    .min(-10)
+    .max(10)
+    .step(0.001)
+    .name('lightX');
+
+gui
+    .add(directionalLight.position, 'y')
+    .min(-10)
+    .max(10)
+    .step(0.001)
+    .name('lightY');
+
+gui
+    .add(directionalLight.position, 'z')
+    .min(-10)
+    .max(10)
+    .step(0.001)
+    .name('lightZ');
+
+// Shadow
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+// directionalLight.shadow.mapSize.set(1024, 1024);
+/**
+ * `shadow`가 만들어낼 `texture`의 크기 설정
+ * => 값이 커질수록 날카로운 그림자가 연출되지만, 성능을 많이 사용하게 된다.
+ * => 기본값인 `512, 512` 를 그대로 사용하는 것을 추천한다.
+ * 
+ * => 날카로운 그림자를 의도한다면, `1024, 1024` 정도로 사용하자
+ * 
+ * 렌더링되는 물체가 복잡할수록 그림자가 흐리게 나타나는 것을 발견하지 못하게 된다.
+ * => 이러한 경우라면 `256, 256` 으로 낮추는 것으로 성능까지 향상시키는 방법이 있다.
+ */
+directionalLight.shadow.mapSize.set(512, 512);
+
+gui.add(directionalLight, 'castShadow');
+
+/**
+ * `Shadow Acne` 현상 (=== `Self-Shadowing`)
+ * => Object 자신이 생성한 그림자가 자신에게 비춰지는 원리에 의해,
+ * => `주름`이 렌더링되는 현상이다.
+ * 
+ * 해결하기 위해서는 아래의 2가지 속성을 함께 사용하여 해결할 수 있다.
+ * * `Light.shadow.bias`
+ * * `Light.shadow.normalBias`
+ */
+
+/**
+ * `Light.shadow.normalBias` 설정
+ * => 이 Light 가 생성하는 shadow의 크기를 조정한다.
+ */
+directionalLight.shadow.normalBias = 0.027;
+gui
+    .add(directionalLight.shadow, 'normalBias')
+    .min(-0.05)
+    .max(0.05)
+    .step(0.001);
+
+/**
+ * `Light.shadow.bias` 설정
+ * => 이 Light 가 생성하는 shadow의 위치(offset)을 조정한다.
+ */
+directionalLight.shadow.bias = - 0.004;
+gui
+    .add(directionalLight.shadow, 'bias')
+    .min(-0.05)
+    .max(0.05)
+    .step(0.001);
+
+// Helper
+// const directionalLightHelper = new THREE.CameraHelper(
+//     directionalLight.shadow.camera
+// );
+// scene.add(directionalLightHelper);
+
+// Target
+directionalLight.target.position.set(0, 4, 0);
+/**
+ * `DirectionalLight.target.position`을 변경해도 화면에 반영되지 않는 현상
+ * 
+ * * `DirectionalLight.target` 은 `Object3D` 타입이지만, 더미이며, Scene 에 등록하지 않는다.
+ * * `position`을 변경하는 것은 `Matrix`를 변경한다는 것인데, `Matrix`는 이 객체가 렌더링되기 직전에 생성된다.
+ * * => `target`은 Scene 에 등록하지 않았으므로, 렌더링 자체를 하지 않으므로, `Matrix`를 만드는 시점은 오지 않는다.
+ * 
+ * 그래서 아래와 같이 직접 `Matrix`를 생성하도록 `directionalLight.target.updateWorldMatrix()`를 호출해야 렌더링에 반영된다.
+ */
+directionalLight.target.updateWorldMatrix();
+
+/**
  * Models
  */
-// Helmet
-gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(10, 10, 10)
-        scene.add(gltf.scene)
+// // Helmet
+// gltfLoader.load(
+//     '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+//     (gltf) =>
+//     {
+//         gltf.scene.scale.set(10, 10, 10)
+//         scene.add(gltf.scene)
 
-        updateAllMaterials()
-    }
-)
+//         updateAllMaterials()
+//     }
+// )
+
+// Hamburger
+gltfLoader.load('/models/hamburger.glb', gltf => {
+    gltf.scene.scale.setScalar(0.4);
+    gltf.scene.position.set(0, 2.5, 0);
+    scene.add(gltf.scene);
+    updateAllMaterials();
+});
+
+/**
+ * Floor
+ */
+const floorColorTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_diff_1k.jpg');
+const floorNormalTexture = textureLoader.load('textures/wood_cabinet_worn_long/wood_cabinet_worn_long_nor_gl_1k.png');
+const floorAORoughnessMetalnessTexture = textureLoader.load('textures/wood_cabinet_worn_long/wood_cabinet_worn_long_arm_1k.jpg');
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 8),
+    new THREE.MeshStandardMaterial({
+        map: floorColorTexture,
+        normalMap: floorNormalTexture,
+        aoMap: floorAORoughnessMetalnessTexture,
+        roughnessMap: floorAORoughnessMetalnessTexture,
+        metalnessMap: floorAORoughnessMetalnessTexture,
+    })
+);
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Wall
+ */
+const wallColorTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_diff_1k.jpg');
+const wallNormalTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_nor_gl_1k.png');
+const wallAORoughnessMetalnessTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_arm_1k.jpg');
+
+wallColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+const wall = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 8),
+    new THREE.MeshStandardMaterial({
+        map: wallColorTexture,
+        normalMap: wallNormalTexture,
+        aoMap: wallAORoughnessMetalnessTexture,
+        roughnessMap: wallAORoughnessMetalnessTexture,
+        metalnessMap: wallAORoughnessMetalnessTexture,
+    })
+);
+wall.position.y = 4;
+wall.position.z = -4;
+scene.add(wall);
 
 /**
  * Sizes
@@ -130,6 +293,10 @@ gui.add(renderer, 'toneMapping', {
     ACESFilmic: THREE.ACESFilmicToneMapping,
 });
 gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001);
+
+// Shadow
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 /**
  * Animate
